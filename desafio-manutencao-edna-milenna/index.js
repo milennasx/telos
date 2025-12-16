4b9a7664d5613a814189fcdc688247aa42e355bc
Milenna Xavier <milennasx@gmail.com>
2025-12-16 17:08:13 -0300

    entrega do simulador 2

---
+++ desafio-manutencao-edna-milenna/index.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    var botao = document.getElementById('btn-aviso');
+
+    botao.addEventListener('click', function() {
+        alert("Você clicou no botão!");
+    });
+
+});
